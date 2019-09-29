4d03a2bd50944cbbc456f5ca1999d17662a382b4
Andy Ruiz Garramones <andy.garramones@corp.globo.com>
2019-09-29 02:34:07 -0300

    Complete implementation for offside calculation and draw

--- offsideLine.py
+++ offsideLine.py
@@ -1,84 +1,97 @@
 import cv2
 import numpy as np
 
+HEIGHT = 75
 
-def homography_2d(x, y, u, v):
+# Camera matrix to relate image (2D) points with world (2D) points
+# matrix =
+#     [x[0], y[0], 1, 0, 0, 0, -u[0] * x[0], - u[0] * y[0], -u[0]],
+#     [0, 0, 0, x[0], y[0], 1, -v[0] * x[0], - v[0] * y[0], -v[0]],
+#     [x[1], y[1], 1, 0, 0, 0, -u[1] * x[1], - u[1] * y[1], -u[1]],
+#     [0, 0, 0, x[1], y[1], 1, -v[1] * x[1], - v[1] * y[1], -v[1]]...
+#
+def compute_homography_2d(u, v, x, y):
+    # Fulfill a matrix with the equations that determine the camera matrix
+    matrix = []
+    for i in range(len(x)):
+        matrix.append([x[i], y[i], 1, 0, 0, 0, -u[i] * x[i], - u[i] * y[i], -u[i]])
+        matrix.append([0, 0, 0, x[i], y[i], 1, -v[i] * x[i], - v[i] * y[i], -v[i]])
+
+    #We minimize || Am || subject to ||m|| = 1 using svd method
+    matrix = np.array(matrix)
+    u, s, vh = np.linalg.svd(matrix)
+    last_line_vh = vh[-1]
+
+    #The 8 values from the last row of Vh are our answer values
+    # so we reconstruct our homography matrix from these values
     h = np.array([
-        [x[0], y[0], 1, 0, 0, 0, -u[0] * x[0], - u[0] * y[0]],
-        [0, 0, 0, x[0], y[0], 1, -v[0] * x[0], - v[0] * y[0]],
-        [x[1], y[1], 1, 0, 0, 0, -u[1] * x[1], - u[1] * y[1]],
-        [0, 0, 0, x[1], y[1], 1, -v[1] * x[1], - v[1] * y[1]],
-        [x[2], y[2], 1, 0, 0, 0, -u[2] * x[2], - u[2] * y[2]],
-        [0, 0, 0, x[2], y[2], 1, -v[2] * x[2], - v[2] * y[2]],
-        [x[3], y[3], 1, 0, 0, 0, -u[3] * x[3], - u[3] * y[3]],
-        [0, 0, 0, x[3], y[3], 1, -v[3] * x[3], - v[3] * y[3]],
-    ])
-    b = np.array([u[0], v[0], u[1], v[1], u[2], v[2], u[3], v[3]])
+        [vh[8][0], vh[8][1], vh[8][2]],
+        [vh[8][3], vh[8][4], vh[8][5]],
+        [vh[8][6], vh[8][7], vh[8][8]],
+        ])
 
-    m = np.linalg.solve(h, b)
-    p = np.array([[m[0], m[1], m[2]], [m[3], m[4], m[5]], [m[6], m[7], 1]])
-    return p
-
+    return h
 
 def calculate_transformation_matrix():
+    # We define our calibration points in world and image coordinates
+    world_points = np.array([[0, 0], [0, HEIGHT], [11, 0], [11, HEIGHT]])
+    image_points = np.array([[269, 25], [266, 346], [440, 27], [586, 346]])
 
-    pts_src = np.array([[269, 23], [264, 344], [439, 23], [585, 346]])
-    pts_dst = np.array([[0, 0], [0, 68], [11, 0], [11, 68]])
+    x = world_points[:, 0]
+    y = world_points[:, 1]
+    u = image_points[:, 0]
+    v = image_points[:, 1]
 
-    x = pts_dst[:, 0]
-    y = pts_dst[:, 1]
-    u = pts_src[:, 0]
-    v = pts_src[:, 1]
+    return compute_homography_2d(u, v, x, y)
 
-    return homography_2d(x, y, u, v)
+def transformate_point(point, transformation_matrix):
+    #We transform the point to homogeneous coordinates adding one more dimension with value 1 to our vector
+    homogeneous_coordinates = np.array([point[0], point[1], 1])
+    dot_product = np.dot(transformation_matrix, homogeneous_coordinates)
+    s = dot_product[-1] # S = last element of the array
+    coordinates = dot_product / s # (x/s, y/s, 1)
+    x,y = coordinates[0], coordinates[1]
+    return (x,y)
 
+def calculate_offside_points(matrix,x,y):
+    #Calculates inverse matrix to transform from image to world coordinates
+    inverse_matrix = np.linalg.inv(matrix)
 
-def calculateOffsidePoints(matrix,x,y):
-    inverseMatrix = np.linalg.inv(matrix)
-    homogeneousCoordinates = np.array([x,y,1])
-    dotProduct = np.dot(inverseMatrix,homogeneousCoordinates)
-    s = dotProduct[-1]
-    coordinates = dotProduct / s # (x/s, y/s, 1)
-    x = coordinates[0]
+    #Tansforms the selected point from image to world coordinates
+    world_point = transformate_point((x,y), inverse_matrix)
+    x = world_point[0]
 
-    point1 = np.array([x,0,1])
-    point2 = np.array([x,68,1])
+    #To draw the offside line we will have to find 2 points
+    #for that we simply mantain const our x of the world coordinate and use the minimum and
+    #maximum possible value for y. So we have a straight line crossing our selected point
+    point1 = np.array([x, 0])
+    point2 = np.array([x, HEIGHT])
 
-    dotProduct = np.dot(matrix,point1)
-    s = dotProduct[-1]
-    coordinates = dotProduct / s # (x/s, y/s, 1)
-    coordinate1 = coordinates[:-1]
+    # Now we transform each of that 2 points to image coordinates
+    offside_point_1 = transformate_point(point1, matrix)
+    offside_point_2 = transformate_point(point2, matrix)
 
-    dotProduct = np.dot(matrix,point2)
-    s = dotProduct[-1]
-    coordinates = dotProduct / s # (x/s, y/s, 1)
-    coordinate2 = coordinates[:-1]
-
-    coordinates = []
-    coordinates.append(np.round(coordinate1,0).astype(int))
-    coordinates.append(np.round(coordinate2,0).astype(int))
-
-    return coordinates[0][0],coordinates[0][1],coordinates[1][0],coordinates[1][1]
+    return int(round(offside_point_1[0])), int(round(offside_point_1[1])), int(round(offside_point_2[0])), int(round(offside_point_2[1]))
 
 
-def mouse_drawing(event, x, y, flags, params):
+def mouse_drawing(event, x, y, flags, data):
     if event == cv2.EVENT_LBUTTONDOWN:
-        editedImage = params["image"].copy()
-        x1,y1,x2,y2 = calculateOffsidePoints(params["matrix"],x,y)
+        editedImage = data["image"].copy()
+        x1,y1,x2,y2 = calculate_offside_points(data["matrix"], x, y)
         print(x1,y1,x2,y2)
-        cv2.line(editedImage,(x1,y1),(x2,y2),(0,0,255),2)
+        cv2.line(editedImage,(x1, y1), (x2, y2), (0, 0, 255), 2)
         cv2.imshow("Maracana", editedImage)
 
 
 
 # função main em python
 if __name__ == '__main__' :
+    #We define a dic with our transformation matrix and image
+    data = {}
+    data["matrix"] = calculate_transformation_matrix()
+    data["image"] = cv2.imread('maracana2.jpg') # Carrega e mostra a imagem
 
-    params = {}
-    params["matrix"] = calculate_transformation_matrix()
-    params["image"] = cv2.imread('maracana2.jpg') # Carrega e mostra a imagem
-
-
-    cv2.imshow("Maracana", params["image"])
-    cv2.setMouseCallback("Maracana", mouse_drawing, params)# Seta callback para quando for apertado com mouse
-    key = cv2.waitKey(0) # Fecha janela grafica quando aperta ESC
+    # We show the image and set the callback to draw the line when clicked on the image
+    cv2.imshow("Maracana", data["image"])
+    cv2.setMouseCallback("Maracana", mouse_drawing, data)
+    key = cv2.waitKey(0)
